201894a19ccbe80cfe9b9eaf537cf3734b1cafdf
mock <mockismymain@gmail.com>
2024-04-21 17:16:11 -0300

    CORSJS adicionado e configurado a API, no app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-const router = require('./router')
+const cors = cors();
+const router = require('./router');
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
+
+app.options({"origin": "*",
+"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+"preflightContinue": false,
+"optionsSuccessStatus": 204}, cors());
 
 module.exports = app;
